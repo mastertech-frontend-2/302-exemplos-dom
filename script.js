12628cd6ab947ad9b0cf9078c3e1ebf0f3deebdd
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-10-09 20:06:42 -0300

    Adicionar animacao ao botao do menu hamburguer

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector('section nav');
 const botaoMenu = document.querySelector('#menu-hamburguer');
+const tracinhos = document.querySelectorAll('.tracinho');
 
 botaoMenu.onclick = () => {
   menu.classList.toggle('visible');
+  
+  for (const tracinho of tracinhos) {
+    tracinho.classList.toggle('visible');
+  }
 }
